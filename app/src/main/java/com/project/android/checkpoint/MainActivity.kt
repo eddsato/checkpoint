68ca05b105eb3c7bc709986b0511b5d5640f4ac5
Eduardo Sato <eduardosato@live.com>
2019-05-11 10:50:16 -0300

    first commit

---
+++ app/src/main/java/com/project/android/checkpoint/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.project.android.checkpoint
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar_main))
+    }
+}
