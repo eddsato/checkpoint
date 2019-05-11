bf8005f198e2a8f9c5ef9782b0fe6d2ff8c48913
Eduardo Sato <eduardosato@live.com>
2019-05-11 15:33:29 -0300

    add a floating and actions buttons for the main activity

--- app/src/main/java/com/project/android/checkpoint/MainActivity.kt
+++ app/src/main/java/com/project/android/checkpoint/MainActivity.kt
@@ -2,6 +2,7 @@ package com.project.android.checkpoint
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         viewpager_main.adapter = fragmentAdapter
 
         tablayout_main.setupWithViewPager(viewpager_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_activity_actions, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
